2e040bd143c783217d5a644aaf8294f9e68384ae
yogipuji15 <yogipuji.yps@gmail.com>
2023-11-27 23:18:15 +0700

    feat(login-signup): Fix login signup UI

--- app/src/main/java/com/example/pesenin/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/pesenin/data/repository/UserRepository.kt
@@ -8,11 +8,10 @@ import com.google.firebase.database.FirebaseDatabase
 class UserRepository {
     private lateinit var database:DatabaseReference
 
-fun CreateUser(email:String, name:String, isVerified:Boolean, ktm:String, password:String, Uid:String, callback: (Boolean)->Unit){
+fun CreateUser(email:String,nim:String, phone:String, profil:String, password:String, Uid:String, callback: (Boolean)->Unit){
         database = FirebaseDatabase.getInstance().getReference("Users")
 
-        val User = User(email, name, isVerified,ktm, password)
-
+        val User = User(email, nim, phone,profil, password)
 
         database.child(Uid).setValue(User).addOnCompleteListener { task ->
             if (task.isSuccessful) {
